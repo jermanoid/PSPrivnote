6e4363a1b6ba1b722d5d5021dcb5b0313cbbc5bb
jermanoid <accounts@jermanoid.com>
2021-08-17 16:47:49 -0400

    Add files via upload

---
+++ privnotesend.ps1
@@ -0,0 +1,21 @@
+﻿function privnote($message){
+    Write-Output "Generating Privnote link"
+    $url = "https://privnote.com"
+    $ie = New-Object -com InternetExplorer.Application
+    $ie.visible = $false
+    $ie.navigate($url)
+    while($ie.Busy){Start-Sleep -milliseconds 100}
+    $doc = $ie.Document
+    $input = $doc.getElementByID("note_raw")
+    $encbutton = $doc.getElementByID("encrypt_note")
+    $input.value = $message
+    $encbutton.click()
+
+    Start-Sleep -seconds 5
+
+    $output = $doc.getElementByID("note_link_input")
+    $link = $output.value
+    write-output $link
+   
+    $ie.quit()
+}
